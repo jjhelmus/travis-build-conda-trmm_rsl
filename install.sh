311292cdcce247f68c301efb29779bb3da2f0522
Jonathan Helmus <jjhelmus@gmail.com>
2015-02-04 19:05:20 -0600

    Remove homebrew libjpeg

--- install.sh
+++ install.sh
@@ -7,3 +7,6 @@ export PATH=/Users/travis/miniconda/bin:$PATH
 
 conda update --yes conda
 conda install --yes conda-build jinja2 binstar
+
+# remove Homebrew libjpeg or else librsl links against it.
+rm /usr/local/lib/libjpeg.8.dylib
